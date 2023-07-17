7ed83bd52dd16779f2556de26e24adaee9275d44
Fikri Putra Hidayat <93188486+ApolloFikz13@users.noreply.github.com>
2023-07-17 13:42:59 +0700

    Update app.py

--- app.py
+++ app.py
@@ -7,8 +7,9 @@ import tempfile
 
 mp_holistic = mp.solutions.holistic  # Holistic model
 
-actions = ["halo","nama","saya","kamu","siapa"]
-model = load_model('realtimeV12.h5')
+#actions = ["halo","nama","saya","kamu","siapa"] #realtimemodel
+actions = ['di', 'halo', 'J', 'kamu', 'ke', 'mana', 'nama', 'saya', 'siapa', 'Z'] #KFold model
+model = load_model('kfold_model.h5')
 
 threshold = 0.5
 
@@ -59,7 +60,7 @@ st.markdown(
         height: 35px;
     }
     .sentence {
-        font-size: 34px;
+        font-size: 21px;
     }
     .subheader {
         font-size: 14px;
@@ -69,13 +70,19 @@ st.markdown(
     unsafe_allow_html=True
 )
 
+
 st.markdown('<div class="bar"></div>', unsafe_allow_html=True)
 
 st.markdown('<h1 style="text-align: center;">Sign Language Detection</h1>', unsafe_allow_html=True)
 
-uploaded_file = st.file_uploader("Upload a video file (5 second max.)", type=["mp4"])
+for _ in range(5):
+    st.markdown("")
 
-st.markdown('<h2 style="font-size: 24px;">Note: only detected one gesture at a time!</h2>', unsafe_allow_html=True)
+uploaded_file = st.file_uploader("Unggah video (maks. 5 detik)", type=["mp4"])
+flip_the_video = st.checkbox("cek untuk video dari kamera depan")
+
+st.markdown('<h2 style="font-size: 21px;">Note: hanya dapat mendeteksi satu gerakan dalam satu waktu!</h2>', unsafe_allow_html=True)
+
 
 if uploaded_file is not None:
     with tempfile.NamedTemporaryFile(delete=False) as temp_file:
@@ -92,11 +99,14 @@ if uploaded_file is not None:
         ret, frame = video.read()
         if not ret:
             break
-        frame = cv2.flip(frame, 1)
+        if flip_the_video == True:
+                frame = cv2.flip(frame, 1)
+        elif flip_the_video == False:
+                pass
         transformed_frame = video_transformer.transform(frame)
 
     video.release()
 
-    st.markdown('<h2 style="font-size: 30px;">Detected Word:</h2>', unsafe_allow_html=True)
+    st.markdown('<h2 style="font-size: 27px;">Hasil Deteksi:</h2>', unsafe_allow_html=True)
 
     st.write('<div class="sentence">' + ' '.join(video_transformer.sentence) + '</div>', unsafe_allow_html=True)
